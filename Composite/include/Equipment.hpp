2ce56cd937b142b081bf013653985ea995b91e6b
4me2fun <mariomajoch@gmail.com>
2018-01-26 14:28:52 +0100

    removed spaces in Equipment.hpp

--- Composite/include/Equipment.hpp
+++ Composite/include/Equipment.hpp
@@ -1,17 +1,17 @@
 class Equipment {
 public:
   virtual ~Equipment();
-  
+
   const char* name() { return m_name; }
-  
+
   virtual Watt power();
   virtual Currency netPrice();
   virtual Currency discountPrice();
-  
+
   virtual void add(Equipment*);
   virtual void remove(Equipment*);
   virtual Iterator<Equipment*>* createIterator();
-  
+
 protected:
   Equipment(const char*);
 
