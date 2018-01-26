c67608c9d26b44027b98b1de8a5596426cffaa37
4me2fun <mariomajoch@gmail.com>
2018-01-26 17:27:31 +0100

    buildable Main.cpp

--- Composite/source/Main.cpp
+++ Composite/source/Main.cpp
@@ -1,11 +1,15 @@
-#include <Chassis.hpp>
-#include <FloppyDisk.hpp>
+#include <iostream>
+
+//#include <Chassis.hpp>
+//#include <FloppyDisk.hpp>
 
 int main () {
-  Chassis *chassis = new Chassis( "PC board" );
-  chassis->Add( new FloppyDisk( "Floppy disk 3.5 inch" ) );
+  std::cout << __FUNCTION__ << " program started" << std::endl;
 
-  std::cout << "netto discount equals " << chassis->NetPrice() << std::endl;
+//  Chassis *chassis = new Chassis( "PC board" );
+//  chassis->Add( new FloppyDisk( "Floppy disk 3.5 inch" ) );
+
+//  std::cout << "netto discount equals " << chassis->NetPrice() << std::endl;
 
   return 0;
 }
